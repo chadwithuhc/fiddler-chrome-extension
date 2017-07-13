90817bff5b052ff157a432f32c0257aa5d01a0f3
chadwithuhc <chadwithuhc@gmail.com>
2017-07-13 14:28:20 -0600

    Presentation mode and editor font size adjustments

---
+++ changelog.md
@@ -0,0 +1,16 @@
+# v1.1 -- TBD
+
+- [x] Presentation Mode
+  - [x] Increase editor font size
+  - [ ] BUG: Horizontal sizing bar unclickable in Presentation Mode
+- [ ] Allow adjusting editor font size
+  - [ ] Save my font size options
+  - [ ] Track "Presentation Mode" and standard font sizes separately
+
+# v1.0 -- 7/12/17
+
+- [x] Show/Hide Sidebar
+- [x] Show/Hide HTML Panel
+- [x] Show/Hide CSS Panel
+- [x] Show/Hide JavaScript Panel
+- [x] Show/Hide Result Panel
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function addSettingsToPage() {
 
 	const template = `
 		<label class="checkboxCont">
-			<input type="checkbox" name="{name}" checked="{checked}" data-action="{action}" data-opt="{opt}">
+			<input type="checkbox" name="{name}" {checked} data-action="{action}" data-opt="{opt}">
 			<span class="checkbox"><i class="bts bt-check"></i></span>
 			{label}
 		</label>
@@ -15,36 +15,42 @@ function addSettingsToPage() {
 
 	const options = [
 		{
+			name: 'showPresentationMode',
+			label: 'Presentation Mode',
+			checked: '',
+			action: 'togglePresentationMode'
+		},
+		{
 			name: 'showSidebar',
 			label: 'Show Sidebar',
-			checked: 'checked',
+			checked: 'checked="checked"',
 			action: 'toggleSidebar'
 		},
 		{
 			name: 'showHTMLPanel',
 			label: 'Show HTML Panel',
-			checked: 'checked',
+			checked: 'checked="checked"',
 			action: 'togglePanel',
 			opt: 'html'
 		},
 		{
 			name: 'showCSSPanel',
 			label: 'Show CSS Panel',
-			checked: 'checked',
+			checked: 'checked="checked"',
 			action: 'togglePanel',
 			opt: 'css'
 		},
 		{
 			name: 'showJavaScriptPanel',
 			label: 'Show JavaScript Panel',
-			checked: 'checked',
+			checked: 'checked="checked"',
 			action: 'togglePanel',
 			opt: 'js'
 		},
 		{
 			name: 'showResultPanel',
 			label: 'Show Result Panel',
-			checked: 'checked',
+			checked: 'checked="checked"',
 			action: 'togglePanel',
 			opt: 'result'
 		}
@@ -73,7 +79,7 @@ function addSettingsToPage() {
 	})
 }
 
-const Fiddler = {
+const Fiddler = document.Fiddler = {
 
 	toggleSidebar() {
 		document.body.classList.toggle('fiddler--no-sidebar')
@@ -86,6 +92,21 @@ const Fiddler = {
 		result: true
 	},
 
+	presentationMode: {
+		enabled: false,
+		editorFontSize: 18
+	},
+
+	editorFontSize: {
+		max: 18,
+		min: 14,
+		base: 14,
+		value: 14,
+		isValid(value) {
+			return value <= this.max && value >= this.min
+		}
+	},
+
 	togglePanel(panel) {
 		if (!panel) {
 			return
@@ -94,6 +115,36 @@ const Fiddler = {
 		Fiddler.panelStates[panel] = !Fiddler.panelStates[panel]
 
 		document.body.classList.toggle(`fiddler--no-${panel}-panel`)
+	},
+
+	togglePresentationMode() {
+		Fiddler.presentationMode.enabled = !Fiddler.presentationMode.enabled
+		document.body.classList.toggle(`fiddler--presentation-mode`)
+
+		Fiddler.setEditorFontSize(
+			Fiddler.presentationMode.enabled ?
+				Fiddler.presentationMode.editorFontSize
+				: Fiddler.editorFontSize.base
+		)
+	},
+
+	adjustEditorFontSize(amount) {
+		const newFontSize = Fiddler.editorFontSize.value + amount
+		if (!Fiddler.editorFontSize.isValid(newFontSize)) {
+			return
+		}
+
+		document.body.classList.remove(`fiddler--editor-font-size-${Fiddler.editorFontSize.value}`)
+		document.body.classList.add(`fiddler--editor-font-size-${newFontSize}`)
+	},
+
+	setEditorFontSize(newFontSize) {
+		if (!Fiddler.editorFontSize.isValid(newFontSize)) {
+			return
+		}
+
+		document.body.classList.remove(`fiddler--editor-font-size-${Fiddler.editorFontSize.value}`)
+		document.body.classList.add(`fiddler--editor-font-size-${Fiddler.editorFontSize.value = newFontSize}`)
 	}
 
 }
